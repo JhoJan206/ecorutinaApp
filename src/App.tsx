9496bf79ca2c57e95961664e1732d8d074fc0a03
JhoJan206 <jacomejhojan2006@gmail.com>
2026-04-12 10:46:39 -0500

    Version inicial del frontend del menu o home de la web, se agrego home.tsx, home.css y modifico el App.tsx para agregar el home al router principal

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -29,7 +28,6 @@ import '@ionic/react/css/display.css';
 /* import '@ionic/react/css/palettes/dark.always.css'; */
 /* import '@ionic/react/css/palettes/dark.class.css'; */
 import '@ionic/react/css/palettes/dark.system.css';
-
 /* Theme variables */
 import './theme/variables.css';
 
@@ -38,6 +36,7 @@ setupIonicReact();
 import Portada from './pages/Portada';
 import Login from './pages/Login';
 import Registro from './pages/Registro';
+import Home from './pages/Home';
 
 const App: React.FC = () => (
   <IonApp>
@@ -46,6 +45,7 @@ const App: React.FC = () => (
         <Route exact path="/" component={Portada} />
         <Route path="/login" component={Login} />
         <Route path="/registro" component={Registro} />
+        <Route path="/home" component={Home} />
       </IonRouterOutlet>
     </IonReactRouter>
   </IonApp>
