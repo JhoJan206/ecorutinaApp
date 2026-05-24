44b149c14a299cf5d525fb005de74cf88f6165d8
JhoJan206 <jacomejhojan2006@gmail.com>
2026-05-24 15:55:42 -0500

    fix: agregar MYSQL_ROOT_PASSWORD como fallback

--- backend/bd.js
+++ backend/bd.js
@@ -8,7 +8,7 @@ dotenv.config();
 const conexion = mysql.createConnection({
     host: process.env.MYSQL_HOST || process.env.MYSQLHOST || process.env.DB_HOST || "127.0.0.1",
     user: process.env.MYSQL_USER || process.env.MYSQLUSER || process.env.DB_USER || "root",
-    password: process.env.MYSQL_PASSWORD || process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || "",
+    password: process.env.MYSQL_ROOT_PASSWORD || process.env.MYSQL_PASSWORD || process.env.MYSQLPASSWORD || process.env.DB_PASSWORD || "",
     database: process.env.MYSQL_DATABASE || process.env.MYSQLDATABASE || process.env.DB_NAME || "ecoRutina",
     port: parseInt(process.env.MYSQL_PORT || process.env.MYSQLPORT || process.env.DB_PORT || "3306"),
     dateStrings: true
